ce6dfcecba55734fac382e08f7867917511a5eb1
Angelica Kelley <akelley@digitalaustin.com>
2018-02-08 13:14:01 -0600

    module import paths for adv srch

--- src/app/pages/main/main.module.js
+++ src/app/pages/main/main.module.js
@@ -7,10 +7,10 @@ import uiBootstrap from 'angular-ui-bootstrap';
 import 'angular-pageslide-directive/src/angular-pageslide-directive';
 import 'angularjs-dropdown-multiselect/dist/src/angularjs-dropdown-multiselect';
 import 'jquery/dist/jquery';
-import 'angular-advanced-searchbox/bower_components/bootstrap/dist/js/bootstrap';
 import 'angular-animate/angular-animate';
-import 'angular-advanced-searchbox/bower_components/bootstrap/dist/css/bootstrap.css'; 
-import 'angular-advanced-searchbox/dist/angular-advanced-searchbox-tpls';
+import '../../../../bower_components/bootstrap/dist/js/bootstrap';
+import '../../../../bower_components/bootstrap/dist/css/bootstrap.css'; 
+import '../../../../bower_components/angular-advanced-searchbox/dist/angular-advanced-searchbox-tpls';
 
 
 
