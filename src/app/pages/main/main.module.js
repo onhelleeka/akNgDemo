291f3d1d227c5622eeb0c659bfb8675cc54eee9c
Angelica Kelley <akelley@digitalaustin.com>
2018-02-08 15:23:11 -0600

    netlify fixes

--- src/app/pages/main/main.module.js
+++ src/app/pages/main/main.module.js
@@ -5,15 +5,14 @@ import MainComponent from './main.component';
 // akdemo specific modules
 import uiBootstrap from 'angular-ui-bootstrap';
 import 'angular-pageslide-directive/src/angular-pageslide-directive';
-import 'jquery/dist/jquery';
 import 'angularjs-dropdown-multiselect/dist/src/angularjs-dropdown-multiselect';
 
-//import '../../../../bower_components/jquery/dist/jquery';
-/* import 'angular-animate/angular-animate';
+import '../../../../bower_components/jquery/dist/jquery';
 import '../../../../bower_components/bootstrap/dist/js/bootstrap';
+import 'angular-animate/angular-animate.min';
 import '../../../../bower_components/bootstrap/dist/css/bootstrap.css'; 
 import '../../../../bower_components/angular-advanced-searchbox/dist/angular-advanced-searchbox-tpls';
- */
+
 const mainPageModule = angular.module('main-module', [
     'ui.router',uiBootstrap, 'pageslide-directive','angularjs-dropdown-multiselect',
     //'angular-advanced-searchbox'
