740ac5b44c772caf81d18357a4e59847b279a908
Angelica Kelley <akelley@digitalaustin.com>
2018-02-08 12:58:47 -0600

    adding adv-srch-box back in

--- src/app/pages/main/main.module.js
+++ src/app/pages/main/main.module.js
@@ -6,17 +6,17 @@ import MainComponent from './main.component';
 import uiBootstrap from 'angular-ui-bootstrap';
 import 'angular-pageslide-directive/src/angular-pageslide-directive';
 import 'angularjs-dropdown-multiselect/dist/src/angularjs-dropdown-multiselect';
-/* import 'jquery/dist/jquery';
-import 'bootstrap/dist/js/bootstrap';
+import 'jquery/dist/jquery';
+import 'angular-advanced-searchbox/bower_components/bootstrap/dist/js/bootstrap';
 import 'angular-animate/angular-animate';
-import 'bootstrap/dist/css/bootstrap.css';
-import '../bower_components/angular-advanced-searchbox/dist/angular-advanced-searchbox-tpls';
- */
+import 'angular-advanced-searchbox/bower_components/bootstrap/dist/css/bootstrap.css'; 
+import 'angular-advanced-searchbox/dist/angular-advanced-searchbox-tpls';
+
 
 
 const mainPageModule = angular.module('main-module', [
     'ui.router',uiBootstrap, 'pageslide-directive','angularjs-dropdown-multiselect',
-    //'angular-advanced-searchbox'
+    'angular-advanced-searchbox'
 ])
     .config(($stateProvider, $urlRouterProvider) => {
         'ngInject';
