02a5bd44e18ae5eacfb3a519956b51a2d9ee417a
Angelica Kelley <akelley@digitalaustin.com>
2018-02-12 03:54:26 -0600

    added gridster

--- src/app/pages/main/main.module.js
+++ src/app/pages/main/main.module.js
@@ -6,19 +6,23 @@ import angular from 'angular';
 // akdemo specific modules
 import uiBootstrap from 'angular-ui-bootstrap';
 
+import 'javascript-detect-element-resize/detect-element-resize';
+import 'javascript-detect-element-resize/jquery.resize';
+import 'angular-gridster/src/angular-gridster';
 
-import 'jquery/dist/jquery';
+/* import 'jquery/dist/jquery';
 import 'bootstrap/dist/js/bootstrap';
 import 'angular-animate/angular-animate.min';
 import 'bootstrap/dist/css/bootstrap.css'; 
 import 'angular-advanced-searchbox/dist/angular-advanced-searchbox-tpls';
-
+ */
 import 'angularjs-dropdown-multiselect/dist/src/angularjs-dropdown-multiselect';
 import 'angular-pageslide-directive/src/angular-pageslide-directive';
 
 const mainPageModule = angular.module('main-module', [
     'ui.router',uiBootstrap, 'pageslide-directive','angularjs-dropdown-multiselect',
-    'angular-advanced-searchbox'
+    'gridster'
+    //'angular-advanced-searchbox'
 ])
     .config(($stateProvider, $urlRouterProvider) => {
         'ngInject';
